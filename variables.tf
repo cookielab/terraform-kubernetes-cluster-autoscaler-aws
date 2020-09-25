faa0c25f739f163d53cf7e940e71aaa2c6b987f6
Patrik Votoček <patrik@votocek.cz>
2020-09-26 00:03:14 +0200

    update default image to 1.15.7

--- variables.tf
+++ variables.tf
@@ -48,12 +48,12 @@ variable "kubernetes_resources_labels" {
 
 variable "kubernetes_deployment_image_registry" {
   type = string
-  default = "k8s.gcr.io/cluster-autoscaler"
+  default = "k8s.gcr.io/autoscaling/cluster-autoscaler"
 }
 
 variable "kubernetes_deployment_image_tag" {
   type = string
-  default = "v1.15.1"
+  default = "v1.15.7"
 }
 
 variable "kubernetes_deployment_node_selector" {
