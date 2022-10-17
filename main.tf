73d51a50d0aefae20498011c1c2c7be785c457b5
Realfriend's TeamCity <TeamCity@realfriend.ai>
2022-10-17 20:28:59 +0300

    Update storage.k8s.io permissions

--- main.tf
+++ main.tf
@@ -1,161 +1,161 @@
 resource "kubernetes_service_account" "cluster_autoscaler" {
   metadata {
-    name = "${var.kubernetes_resources_name_prefix}cluster-autoscaler"
+    name      = "${var.kubernetes_resources_name_prefix}cluster-autoscaler"
     namespace = var.kubernetes_namespace
-    labels = local.kubernetes_resources_labels
+    labels    = local.kubernetes_resources_labels
   }
 }
 
 resource "kubernetes_cluster_role" "cluster_autoscaler" {
   metadata {
-    name = "${var.kubernetes_resources_name_prefix}cluster-autoscaler"
+    name   = "${var.kubernetes_resources_name_prefix}cluster-autoscaler"
     labels = local.kubernetes_resources_labels
   }
 
   rule {
     api_groups = [""]
-    resources = ["events", "endpoints"]
-    verbs = ["create", "patch"]
+    resources  = ["events", "endpoints"]
+    verbs      = ["create", "patch"]
   }
 
   rule {
     api_groups = [""]
-    resources = ["pods/eviction"]
-    verbs = ["create"]
+    resources  = ["pods/eviction"]
+    verbs      = ["create"]
   }
 
   rule {
     api_groups = [""]
-    resources = ["pods/status"]
-    verbs = ["update"]
+    resources  = ["pods/status"]
+    verbs      = ["update"]
   }
 
   rule {
-    api_groups = [""]
-    resources = ["endpoints"]
+    api_groups     = [""]
+    resources      = ["endpoints"]
     resource_names = ["cluster-autoscaler"]
-    verbs = ["get", "update"]
+    verbs          = ["get", "update"]
   }
 
   rule {
     api_groups = [""]
-    resources = ["nodes"]
-    verbs = ["watch", "list", "get", "update"]
+    resources  = ["nodes"]
+    verbs      = ["watch", "list", "get", "update"]
   }
 
   rule {
     api_groups = [""]
-    resources = ["pods", "services", "replicationcontrollers", "persistentvolumeclaims", "persistentvolumes"]
-    verbs = ["watch", "list", "get"]
+    resources  = ["pods", "services", "replicationcontrollers", "persistentvolumeclaims", "persistentvolumes"]
+    verbs      = ["watch", "list", "get"]
   }
 
   rule {
     api_groups = ["extensions"]
-    resources = ["replicasets", "daemonsets"]
-    verbs = ["watch", "list", "get"]
+    resources  = ["replicasets", "daemonsets"]
+    verbs      = ["watch", "list", "get"]
   }
 
   rule {
     api_groups = ["policy"]
-    resources = ["poddisruptionbudgets"]
-    verbs = ["watch", "list"]
+    resources  = ["poddisruptionbudgets"]
+    verbs      = ["watch", "list"]
   }
 
   rule {
     api_groups = ["apps"]
-    resources = ["statefulsets", "replicasets", "daemonsets"]
-    verbs = ["watch", "list", "get"]
+    resources  = ["statefulsets", "replicasets", "daemonsets"]
+    verbs      = ["watch", "list", "get"]
   }
 
   rule {
     api_groups = ["storage.k8s.io"]
-    resources = ["storageclasses", "csinodes"]
-    verbs = ["watch", "list", "get"]
+    resources  = ["storageclasses", "csinodes", "csidrivers", "csistoragecapacities"]
+    verbs      = ["watch", "list", "get"]
   }
 
   rule {
     api_groups = ["batch", "extensions"]
-    resources = ["jobs"]
-    verbs = ["get", "list", "watch", "patch"]
+    resources  = ["jobs"]
+    verbs      = ["get", "list", "watch", "patch"]
   }
   rule {
     api_groups = ["coordination.k8s.io"]
-    resources = ["leases"]
-    verbs = ["create"]
+    resources  = ["leases"]
+    verbs      = ["create"]
   }
   rule {
-    api_groups = ["coordination.k8s.io"]
+    api_groups     = ["coordination.k8s.io"]
     resource_names = ["cluster-autoscaler"]
-    resources = ["leases"]
-    verbs = ["get", "update"]
+    resources      = ["leases"]
+    verbs          = ["get", "update"]
   }
 }
 
 resource "kubernetes_role" "cluster_autoscaler" {
   metadata {
-    name = "${var.kubernetes_resources_name_prefix}cluster-autoscaler"
+    name      = "${var.kubernetes_resources_name_prefix}cluster-autoscaler"
     namespace = kubernetes_service_account.cluster_autoscaler.metadata.0.namespace
-    labels = local.kubernetes_resources_labels
+    labels    = local.kubernetes_resources_labels
   }
 
   rule {
     api_groups = [""]
-    resources = ["configmaps"]
-    verbs = ["create","list","watch"]
+    resources  = ["configmaps"]
+    verbs      = ["create", "list", "watch"]
   }
 
   rule {
-    api_groups = [""]
-    resources = ["configmaps"]
+    api_groups     = [""]
+    resources      = ["configmaps"]
     resource_names = ["cluster-autoscaler-status", "cluster-autoscaler-priority-expander"]
-    verbs = ["delete", "get", "update", "watch"]
+    verbs          = ["delete", "get", "update", "watch"]
   }
 }
 
 resource "kubernetes_cluster_role_binding" "cluster_autoscaler" {
   metadata {
-    name = "${var.kubernetes_resources_name_prefix}cluster-autoscaler"
+    name   = "${var.kubernetes_resources_name_prefix}cluster-autoscaler"
     labels = local.kubernetes_resources_labels
   }
 
   role_ref {
     api_group = "rbac.authorization.k8s.io"
-    kind = "ClusterRole"
-    name = kubernetes_cluster_role.cluster_autoscaler.metadata.0.name
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role.cluster_autoscaler.metadata.0.name
   }
 
   subject {
-    kind = "ServiceAccount"
-    name = kubernetes_service_account.cluster_autoscaler.metadata.0.name
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.cluster_autoscaler.metadata.0.name
     namespace = kubernetes_service_account.cluster_autoscaler.metadata.0.namespace
   }
 }
 
 resource "kubernetes_role_binding" "cluster_autoscaler" {
   metadata {
-    name = "${var.kubernetes_resources_name_prefix}cluster-autoscaler"
+    name      = "${var.kubernetes_resources_name_prefix}cluster-autoscaler"
     namespace = kubernetes_service_account.cluster_autoscaler.metadata.0.namespace
-    labels = local.kubernetes_resources_labels
+    labels    = local.kubernetes_resources_labels
   }
 
   role_ref {
     api_group = "rbac.authorization.k8s.io"
-    kind = "Role"
-    name = kubernetes_role.cluster_autoscaler.metadata.0.name
+    kind      = "Role"
+    name      = kubernetes_role.cluster_autoscaler.metadata.0.name
   }
 
   subject {
-    kind = "ServiceAccount"
-    name = kubernetes_service_account.cluster_autoscaler.metadata.0.name
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.cluster_autoscaler.metadata.0.name
     namespace = kubernetes_service_account.cluster_autoscaler.metadata.0.namespace
   }
 }
 
 resource "kubernetes_deployment" "cluster_autoscaler" {
   metadata {
-    name = "${var.kubernetes_resources_name_prefix}cluster-autoscaler"
+    name      = "${var.kubernetes_resources_name_prefix}cluster-autoscaler"
     namespace = var.kubernetes_namespace
-    labels = local.kubernetes_resources_labels
+    labels    = local.kubernetes_resources_labels
   }
 
   spec {
@@ -167,7 +167,7 @@ resource "kubernetes_deployment" "cluster_autoscaler" {
 
     template {
       metadata {
-        labels = local.kubernetes_deployment_labels
+        labels      = local.kubernetes_deployment_labels
         annotations = var.kubernetes_deployment_annotations
       }
 
@@ -178,15 +178,15 @@ resource "kubernetes_deployment" "cluster_autoscaler" {
 
         container {
           image = local.kubernetes_deployment_image
-          name = "cluster-autoscaler"
+          name  = "cluster-autoscaler"
 
-          resources{
-            limits =  {
-              cpu = "100m"
+          resources {
+            limits = {
+              cpu    = "100m"
               memory = "512Mi"
             }
             requests = {
-              cpu = "100m"
+              cpu    = "100m"
               memory = "512Mi"
             }
           }
@@ -194,15 +194,16 @@ resource "kubernetes_deployment" "cluster_autoscaler" {
           command = local.kubernetes_deployment_container_command
 
           volume_mount {
-            name = "ssl-certs"
+            name       = "ssl-certs"
             mount_path = "/etc/ssl/certs/ca-certificates.crt"
-            read_only = true
+            read_only  = true
           }
 
-          volume_mount { # hack for automountServiceAccountToken
-            name = kubernetes_service_account.cluster_autoscaler.default_secret_name
+          volume_mount {
+            # hack for automountServiceAccountToken
+            name       = kubernetes_service_account.cluster_autoscaler.default_secret_name
             mount_path = "/var/run/secrets/kubernetes.io/serviceaccount"
-            read_only = true
+            read_only  = true
           }
 
           image_pull_policy = "Always"
@@ -215,7 +216,8 @@ resource "kubernetes_deployment" "cluster_autoscaler" {
           }
         }
 
-        volume { # hack for automountServiceAccountToken
+        volume {
+          # hack for automountServiceAccountToken
           name = kubernetes_service_account.cluster_autoscaler.default_secret_name
           secret {
             secret_name = kubernetes_service_account.cluster_autoscaler.default_secret_name
